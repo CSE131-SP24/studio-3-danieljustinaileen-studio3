8cc68bd753a314aab6f78c673db9a53bc30688dc
daniel_kwon <daniel_kwon@10.232.160.3>
2024-02-07 13:32:10 -0600

    first try

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,45 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter an integer: ");
+		int n = in.nextInt();
+		int[] listOfNumbers = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			listOfNumbers[i] = i + 1;
+		}
+		listOfNumbers[0] = 0;
+
+		int index = 0;
+
+		while (index < Math.sqrt(n)) {
+			if (listOfNumbers[index] == 0) {
+				index++;
+			} else {
+				for (int i = 0; i < listOfNumbers.length; i++) {
+					if (listOfNumbers[i] == 0) {
+						
+					} else {
+						if (listOfNumbers[i] / listOfNumbers[index] == 0) {
+							listOfNumbers[i] = 0;
+
+						}
+					}
+				}
+			index++;
+			}
+		}
+		for(int num : listOfNumbers) {
+			if(num != 0) {
+				System.out.print(num + " ");
+			}
+		}
+	}
+
+}
